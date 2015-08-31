3f976becc5642b46ff3bf4ff213930fcb5a5a8db
Abdullah Alger <abdullahalger@me.com>
2015-08-31 20:22:40 +0200

    first test passes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,41 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :timeoutable
+
+  validates :role, presence: true, inclusion: { in: %w{standard pro admin}, message: "should be one of admin, pro, standard" }
+
+  def admin?
+    role == "admin"
+  end
+
+  def pro?
+    role == "pro"
+  end
+
+  def standard?
+    role == "standard"
+  end
+
+  def upgradeable?
+    self.standard?
+  end
+
+  def downgradeable?
+    self.pro?
+  end
+
+  def upgrade_account
+    if self.upgradeable?
+      self.update_attributes(role: "pro")
+    end
+  end
+
+  def downgrade_account
+    if self.downgradeable?
+      self.update_attributes(role: "standard")
+    end
+  end
+
+  # downgradeable saved for users who do not pay the monthly fee. Their account will be downgraded. If users opt out of the monthly payment, they can also choose to downgrade their account.
+
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,11 @@
+require "faker"
+
 FactoryGirl.define do
   factory :user do
-    
+    name Faker::Name.name
+    sequence(:email, 100) { |n| "person#{n}@example.com" }
+    password 'helloworld'
+    password_confirmation "helloworld"
+    confirmed_at Time.now
   end
-
 end
