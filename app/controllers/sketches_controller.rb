179e23c49f812f36c43b9716f0026a4b2495a36e
Abdullah Alger <abdullahalger@me.com>
2015-08-31 22:47:52 +0200

    working on sketches

---
+++ app/controllers/sketches_controller.rb
@@ -0,0 +1,36 @@
+class SketchesController < ApplicationController
+
+  skip_before_action :authenticate_user!, only: [:index]
+
+  def index
+
+  end
+
+  def show
+
+  end
+  
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+
+end
---
+++ spec/models/sketch_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe Sketch, type: :model do
+
+
+end
