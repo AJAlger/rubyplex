5c30b80088ea626ad570b7d91f08f586189bb226
Abdullah Alger <abdullahalger@me.com>
2015-09-03 20:39:34 +0200

    added username to db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150902234128) do
+ActiveRecord::Schema.define(version: 20150903183044) do
 
   create_table "sketches", force: :cascade do |t|
     t.string   "title"
@@ -48,10 +48,12 @@ ActiveRecord::Schema.define(version: 20150902234128) do
     t.integer  "failed_attempts",        default: 0
     t.string   "unlock_token"
     t.datetime "locked_at"
+    t.string   "username"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
