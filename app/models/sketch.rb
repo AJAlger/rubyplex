8f27bfe992fce66c7aa3565fc469f87bdc511888
Abdullah Alger <abdullahalger@me.com>
2015-09-03 11:38:02 +0200

    sketches saving properly

--- app/models/sketch.rb
+++ app/models/sketch.rb
@@ -2,12 +2,12 @@ class Sketch < ActiveRecord::Base
 
   belongs_to :user
 
-  after_create :generate_permalink
-
-  private
-
-  def generate_permalink
-    self.permalink = SecureRandom.urlsafe_base64(10)
-  end
+  # after_create :generate_permalink
+  #
+  # private
+  #
+  # def generate_permalink
+  #   self.permalink = SecureRandom.urlsafe_base64(10)
+  # end
 
 end
