e179cb127ce3de06d191779608465c306dae82ed
Abdullah Alger <abdullahalger@me.com>
2015-09-07 06:14:04 +0200

    rspec test to create sketch passes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :users
   resources :sketches, only: [:new, :create]
 
+  delete ":username/:slug", to: "sketches#delete", as: :sketch_delete
   get ":username/:slug/edit", to: "sketches#edit", as: :sketch_edit
   patch ":username/:slug", to: "sketches#update", as: :sketch_update
   get ":username/:slug", to: "sketches#show", as: :sketch_show
--- spec/features/user_creates_sketch_spec.rb
+++ spec/features/user_creates_sketch_spec.rb
@@ -13,15 +13,16 @@ feature "User creates a sketch" do
     expect(page).to have_button("Save Sketch")
 
     fill_in "Title", with: "A new sketch"
-    fill_in
+    fill_in "input_value", with: "puts 'new sketch'"
 
-    expect(page).to have_content "puts 'hello'"
+    expect(page).to have_content "puts 'new sketch'"
 
     click_button "Save Sketch"
 
-    expect(current_path).to eq sketches_path
+    expect(current_path).to eq sketches_index_path(user.username)
     expect(page).to have_content "A new sketch"
-    expect(page).to have_content "You have 1 sketch"
+    expect(page).to have_content "puts 'new sketch'"
+    expect(page).to have_content "You currently have 1 sketch"
 
   end
 end
