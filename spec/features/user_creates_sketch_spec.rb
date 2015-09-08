5064b7c28868cd63feb2be3ae3676920a753dfce
Eliot Sykes <eliotsykes@gmail.com>
2015-09-08 13:04:23 +0100

    User creates sketch test interacts directly with ACE editor

--- spec/features/user_creates_sketch_spec.rb
+++ spec/features/user_creates_sketch_spec.rb
@@ -13,16 +13,33 @@ feature "User creates a sketch", js: true do
     expect(page).to have_button("Save Sketch")
 
     fill_in "Title", with: "A new sketch"
-    fill_in "input_value", with: "puts 'new sketch'"
+    fill_in_editor_field "puts 'new sketch'"
 
-    expect(page).to have_content "puts 'new sketch'"
+    expect(page).to have_editor_display text: "puts 'new sketch'"
 
     click_button "Save Sketch"
 
     expect(current_path).to eq sketches_index_path(user.username)
     expect(page).to have_content "A new sketch"
-    expect(page).to have_content "puts 'new sketch'"
+    expect(page).to have_editor_display "puts 'new sketch'"
     expect(page).to have_content "You currently have 1 sketch"
 
+  end
+
+  private
+
+  def fill_in_editor_field(text)
+    find_ace_editor_field.set text
+  end
+
+  def find_ace_editor_field
+    input_field_locator = ".ace_text-input"
+    is_input_field_visible = false
+    find(input_field_locator, visible: is_input_field_visible)
+  end
+
+  def have_editor_display(options)
+    editor_display_locator = ".ace_content"
+    have_css(editor_display_locator, options)
   end
 end
