774b05d887b4b0805e918166e0bb087f6adaf82a
Abdullah Alger <abdullahalger@me.com>
2015-09-11 21:07:39 +0200

    spec runs js. next move js to helper

--- spec/features/user_edits_sketch_spec.rb
+++ spec/features/user_edits_sketch_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature "User edits a sketch" do
+feature "User edits a sketch", js: true do
 
   scenario "successfully" do
 
@@ -13,10 +13,40 @@ feature "User edits a sketch" do
 
     click_link (user.username + "/" + sketch.slug)
 
-    expect(current_path).to eq sketch_edit_path(user.username, sketch.slug)
+    #expect(current_path).to eq sketch_edit_path(user.username, sketch.slug)
 
+    expect(page).to have_content("Title")
+    expect(page).to have_button("Save Sketch")
 
+    fill_in "Title", with: "An edited sketch"
+    fill_in_editor_field "puts 'edited sketch'"
 
+    expect(page).to have_editor_display text: "puts 'edited sketch'"
+
+    click_button "Save Sketch"
+
+    expect(current_path).to eq sketches_index_path(user.username)
+    expect(page).to have_content "An edited sketch"
+    expect(page).to have_editor_display text: "puts 'edited sketch'"
+    expect(page).to have_content "You currently have 1 sketch"
+
+  end
+
+  private
+
+  def fill_in_editor_field(text)
+    find_ace_editor_field.set text
+  end
+
+  def find_ace_editor_field
+    input_field_locator = ".ace_text-input"
+    is_input_field_visible = false
+    find(input_field_locator, visible: is_input_field_visible)
+  end
+
+  def have_editor_display(options)
+    editor_display_locator = ".ace_content"
+    have_css(editor_display_locator, options)
   end
 
 end
