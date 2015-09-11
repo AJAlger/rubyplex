2578fb4d1ade6770bf9034089206dfce2b3a319c
Abdullah Alger <abdullahalger@me.com>
2015-09-11 20:57:18 +0200

    spec goes to edit page

--- spec/features/user_edits_sketch_spec.rb
+++ spec/features/user_edits_sketch_spec.rb
@@ -4,8 +4,18 @@ feature "User edits a sketch" do
 
   scenario "successfully" do
 
+    user = create(:user)
+    sketch = create(:sketch, user: user)
+    login_as(user, scope: :user)
 
-    
+    visit sketches_index_path(user.username)
+    expect(page).to have_content(sketch.body)
+
+    click_link (user.username + "/" + sketch.slug)
+
+    expect(current_path).to eq sketch_edit_path(user.username, sketch.slug)
+
+
 
   end
 
