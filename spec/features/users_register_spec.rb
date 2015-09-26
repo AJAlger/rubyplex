76093e1c0a29ad8383308af753279ffc46e02635
Abdullah Alger <abdullahalger@me.com>
2015-09-26 22:39:38 +0200

    fixed rspec test with added username

--- spec/features/users_register_spec.rb
+++ spec/features/users_register_spec.rb
@@ -9,13 +9,14 @@ feature "User registers" do
     click_link "Register"
     expect(current_path).to eq(new_user_registration_path)
 
+    fill_in "Username", with: "jjacob"
     fill_in "Name", with: "John Jacob"
     fill_in "Email", with: "tester@example.tld"
     fill_in "Password", with: "test-password"
     fill_in "Password confirmation", with: "test-password"
     click_button "Sign up"
 
-    expect(current_path).to eq "/"
+    expect(current_path).to eq root_path
     expect(page).to have_content(
                         "A message with a confirmation link has been sent to your email address.
       Please follow the link to activate your account."
