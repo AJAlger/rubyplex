1d20481d65f69c50e84af9f07db87aa154f5bae3
Abdullah Alger <abdullahalger@me.com>
2015-09-28 21:28:02 +0200

    adding rspec tests for the slug

--- spec/models/sketch_spec.rb
+++ spec/models/sketch_spec.rb
@@ -2,5 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Sketch, type: :model do
 
+  before :each do
+    @sketch = create(:sketch)
+  end
+
+  context "#initialize" do
+    it "should have a slug" do
+    end
+
+    it "it should have a slug that's a hex number" do
+
+    end
+  end
 
 end
