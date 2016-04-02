850530955a04bfb5eef25ec5d14531839d145ef7
Abdullah Alger <abdullahalger@me.com>
2016-04-02 23:39:24 +0200

    .

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -4,6 +4,11 @@
 require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
 
+options = {js_errors: false}
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, options)
+end
+
 # Capybara.register_driver :selenium do |app|
 #   Capybara::Selenium::Driver.new(app, :browser => :chrome)
 # end
