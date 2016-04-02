58ad73de203dbbe749be1df788842d7c50f9ae75
Abdullah Alger <abdullahalger@me.com>
2016-04-02 19:02:14 +0200

    added capybara spec to work with chrome

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,5 +1,5 @@
 #Selenium::WebDriver::Firefox::Binary.path = "/Applications/FirefoxDeveloperEdition.app/Contents/MacOS/firefox-bin"
 
-# Capybara.register_driver :selenium do |app|
-#   Capybara::Selenium::Driver.new(app, :browser => :chrome)
-# end
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
