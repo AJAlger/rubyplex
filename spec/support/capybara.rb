115c491e208ad112da55975dc56dd601ddd351e8
Abdullah Alger <abdullahalger@me.com>
2016-04-14 20:10:47 +0200

    ace gem added

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,6 +1,6 @@
 #Selenium::WebDriver::Firefox::Binary.path = "/Applications/FirefoxDeveloperEdition.app/Contents/MacOS/firefox-bin"
 
-
+require 'capybara'
 require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
 
